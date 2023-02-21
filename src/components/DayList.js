a4786f43e878d9b35a6a7d92d17ace062fff1877
Leonardo Picchi Dancona <lpdancona@gmail.com>
2023-02-21 15:35:22 -0800

    adding day list component

---
+++ src/components/DayList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import DayListItem from "./DayListItem";
+export default function DayList(props) {
+  const daysList = props.days.map((day) => {
+    return (
+      <DayListItem
+        key={day.id}
+        name={day.name}
+        spots={day.spots}
+        selected={day.selected}
+        setDay={props.setDay}
+      />
+    );
+  });
+
+  return <ul>{daysList}</ul>;
+}
